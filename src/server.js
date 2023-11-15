1b065d187f84748dc9acc0baca33908e05217480
MarineVega <marinev33@hotmail.com>
2023-11-14 22:22:33 -0300

    Subo estructura del Proyecto

--- src/server.js
+++ src/server.js
@@ -8,9 +8,7 @@ app.get('/',(req, res)=> {
     res.send('Hola Mundo!');
 });
 
-app.get('/usuarios',(req, res)=> {
-    res.send('Lista de usuarios');
-});
+
 
 app.listen(PORT, () => {
   console.log(`Servidor Express corriendo en el puerto ${PORT}`)
